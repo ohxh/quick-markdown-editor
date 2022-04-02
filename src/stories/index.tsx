d0bf04d7bbac42eb46ae50374c03816b50730ba6
Oliver Hopcroft <oliverhopcroft@gmail.com>
2022-04-02 16:12:58 -0400

    First implementation

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { dark, light } from "../styles/theme";
 import Editor from "..";
-
+import Excalidraw from "@excalidraw/excalidraw";
+import initialData from "./initialData";
+import "./style.css";
 const docSearchResults = [
   {
     title: "Hiring",
@@ -57,7 +59,84 @@ class YoutubeEmbed extends React.Component<{
   }
 }
 
+const ExcalidrawEmbed = () => {
+  const excalidrawRef = React.useRef(null);
+
+  const [viewModeEnabled, setViewModeEnabled] = React.useState(false);
+  const [zenModeEnabled, setZenModeEnabled] = React.useState(true);
+  const [gridModeEnabled, setGridModeEnabled] = React.useState(false);
+
+  const updateScene = () => {
+    const sceneData = {
+      elements: [
+        {
+          type: "rectangle",
+          version: 141,
+          versionNonce: 361174001,
+          isDeleted: false,
+          id: "oDVXy8D6rom3H1-LLH2-f",
+          fillStyle: "hachure",
+          strokeWidth: 1,
+          strokeStyle: "solid",
+          roughness: 1,
+          opacity: 100,
+          angle: 0,
+          x: 100.50390625,
+          y: 93.67578125,
+          strokeColor: "#c92a2a",
+          backgroundColor: "transparent",
+          width: 186.47265625,
+          height: 141.9765625,
+          seed: 1968410350,
+          groupIds: [],
+        },
+      ],
+      appState: {
+        viewBackgroundColor: "transparent",
+      },
+    };
+    excalidrawRef.current.updateScene(sceneData);
+  };
+
+  return (
+    <div className="App">
+      <div className="excalidraw-wrapper">
+        <Excalidraw
+          ref={excalidrawRef}
+          initialData={initialData}
+          onChange={(elements, state) =>
+            console.log("Elements :", elements, "State : ", state)
+          }
+          onPointerUpdate={payload => console.log(payload)}
+          onCollabButtonClick={() =>
+            window.alert("You clicked on collab button")
+          }
+          viewModeEnabled={viewModeEnabled}
+          zenModeEnabled={zenModeEnabled}
+          gridModeEnabled={gridModeEnabled}
+        />
+      </div>
+    </div>
+  );
+};
+
 const embeds = [
+  {
+    title: "Drawing",
+    keywords: "excalidraw drawing sketch diagram",
+    defaulHidden: false,
+    icon: () => (
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/75/YouTube_social_white_squircle_%282017%29.svg"
+        width={24}
+        height={24}
+      />
+    ),
+    matcher: url => {
+      return url.match(/a/);
+    },
+    component: ExcalidrawEmbed,
+  },
   {
     title: "YouTube",
     keywords: "youtube video tube google",
